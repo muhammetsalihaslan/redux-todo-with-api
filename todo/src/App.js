b59095ca6fe97d405144feb6db3ae68483a8f9ef
salih <muhammetsalihaslan@gmail.com>
2023-01-10 09:33:27 +0300

    setup

---
+++ todo/src/App.js
@@ -0,0 +1,15 @@
+import Content from "./components/Content";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <section className="todoapp">
+      <Header />
+      <Content />
+      <Footer />
+    </section>
+  );
+}
+
+export default App;
